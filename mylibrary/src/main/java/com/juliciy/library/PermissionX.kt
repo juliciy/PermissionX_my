48821ba02d37877594b4b1579b0d68619c575d8e
juliciy <1343976313@qq.com>
2022-10-29 20:36:46 +0800

    完成PermissionX库的开发与测试工作

---
+++ mylibrary/src/main/java/com/juliciy/library/PermissionX.kt
@@ -0,0 +1,28 @@
+package com.juliciy.library
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+
+/**
+ * Basic interfaces for developers to use PermissionX functions.
+ * @author
+ * @since
+ */
+object PermissionX {
+
+    private const val TAG = "InvisibleFragment"
+
+    fun request(activity: FragmentActivity, vararg permissions: String, callback: PermissionCallback) {
+        val fragmentManager = activity.supportFragmentManager
+        val existedFragment = fragmentManager.findFragmentByTag(TAG)
+        val fragment = if (existedFragment != null) {
+            existedFragment as InvisibleFragment
+        } else {
+            val invisibleFragment = InvisibleFragment()
+            fragmentManager.beginTransaction().add(invisibleFragment, TAG).commitNow()
+            invisibleFragment
+        }
+        fragment.requestNow(callback, *permissions)
+    }
+
+}
